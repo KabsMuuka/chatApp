9c0b02b34a7f73e853e16277c18697bc5ac8fd78
KabsMuuka <kabwalimuuka@gmail.com>
2024-01-20 12:13:15 +0200

    creating real time authentication with googleAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { initialzeApp } from "firebase/app"; //initalizer
+import { getAuth } from "firebase/auth"; //auth
+import { collection } from "firebase/firestore"; //database
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8i19mGZh7ZVqv5e5D7bHgrWECVO6e2go",
+  authDomain: "realtime-d0317.firebaseapp.com",
+  projectId: "realtime-d0317",
+  storageBucket: "realtime-d0317.appspot.com",
+  messagingSenderId: "451790034302",
+  appId: "1:451790034302:web:2001c5c420cfc0d1df0f2f",
+  measurementId: "G-8DJMD56PHS",
+};
+//initialize Firebase
+const app = initialzeApp(firebaseConfig);
+const auth = getAuth();
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import Register from "../components/Register";
 import Login from "../components/Login";
 import Home from "../components/Home";
 
 import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Register />
-    <Login />
-    <Home />
+    <App />
   </React.StrictMode>
 );
